c6a338860b74743f6a125d617cfd8773b9193a62
Dustin Sallings <dustin@spy.net>
2011-08-31 14:15:04 -0700

    Process CPU data.

--- report/viewperf.R
+++ report/viewperf.R
@@ -43,3 +43,41 @@ getTest <- function(r) {
 getUpload <- function(r) {
   getRunData('_design/rviews/_view/by_upload', r)
 }
+
+parseCPUCounters <- function(lines) {
+  ## From the proc(5) man page
+
+  fields <- c('pid', 'comm', 'state', 'ppid', 'pgrp', 'session', 'tty_nr', 'tpgid',
+            'flags', 'minflt', 'cminflt', 'majflt', 'cmajflt', 'utime', 'stime',
+            'cutime', 'cstime', 'priority', 'nice', 'num_threads', 'itrealvalue',
+            'starttime', 'vsize', 'rss', 'rsslim', 'startcode', 'endcode',
+            'startstack', 'kstkesp', 'kstkeip', 'signal', 'blocked', 'sigignore',
+            'sigcatch', 'wchan', 'nswap', 'cnswap', 'exit_signal', 'processor',
+            'rt_priority', 'policy', 'delayacct_blkio_ticks', 'guest_time',
+            'cguest_time')
+
+  s <- plyr::ldply(lines, function(r) unlist(strsplit(r, " ")))
+  names(s) <-  fields[c(1:length(s[1,]))]
+  s$comm <- factor(s$comm)
+  ## Numberize the datas
+  s[,-c(1:3)] <- as.numeric(as.matrix(s[,-c(1:3)]))
+
+  ## Number the lines.
+  s$rowid <- 1:nrow(procstuff)
+  for (comm in levels(s$comm)) {
+    s[s$comm == comm,]$rowid <- 1:nrow(s[s$comm == comm,])
+
+    counters <- c('utime', 'stime', 'cutime', 'cstime')
+    for (counter in counters) {
+      name <- paste(counter, '_diff', sep='')
+      counterdiff <- diff(s[s$comm == comm,counter])
+      s[s$comm == comm, name] <- append(c(0), counterdiff)
+    }
+  }
+
+  s
+}
+
+meltCPUStats <- function(cpudf) {
+  melt(cpudf, id.vars=c('pid', 'comm', 'state', 'rowid'))
+}
