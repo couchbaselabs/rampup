c66b489b9e04bd3701837c6c5cba24abdfcdc53d
farshid <farshid.ghods@gmail.com>
2011-09-30 19:40:26 -0700

    adding a R script to generate short summary
    Change-Id: Ibe4b43d6cd8178cec334cc6b4a1de1f603984e00

---
+++ report/allbuilds.R
@@ -0,0 +1,30 @@
+source("viewperf.R")
+require(ggplot2, quietly=TRUE)
+
+
+prettySize <- function(s, fmt="%.2f") {
+  sizes <- c('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB')
+  e <- floor(log(s, 1024))
+  suffix <- sizes[e+1]
+  prefix <- sprintf(fmt, s/(1024 ^ floor(e)))
+  paste(prefix, suffix, sep="")
+}
+
+
+#listUploads()
+uploadName <- commandArgs(TRUE)
+cat(uploadName)
+pdf(paste(uploadName,sep="",".pdf"))
+#uploadName = "rampup-single-node-memcachetest-3m"
+df <- getUpload(uploadName)
+steps = c("loading-docs","loading-persisted","warming","unclogging")
+for(label in steps) {
+			d <- df[df$label == label,]
+			p <- ggplot(data=d,aes(x=build,y=time,fill=build))
+			p <- p + labs(y='Seconds', x="Build Number") 
+			p <- p + geom_bar(stat='identity', position='stack') 
+			p <- p + geom_text(aes(y = time,vjust=0,hjust=0,size=10,label=paste(sprintf("%.0f",time)," sec")))
+			p <- p + opts(title=paste(comma(d$items),prettySize(d$size, "%d"),"items -", label))
+			print(p)
+}
+dev.off()
