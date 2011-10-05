0773b2faaeec7f798adc4db66fba831e5db2d32c
farshid <farshid.ghods@gmail.com>
2011-10-04 20:31:17 -0700

    made baseline comparison optional
    you can use Rscripts draining.R "upload_id 1.7.1r-68" to
    create a graph which compares all the runs against 1.7.1r-68
    or simply run Rscripts draining.R to get those grapsh without
    the comparison part
    
    Change-Id: I37143d80e60e8c4192403c5155f1d759eda9725b

--- report/draining.R
+++ report/draining.R
@@ -10,21 +10,56 @@ prettySize <- function(s, fmt="%.2f") {
   paste(prefix, suffix, sep="")
 }
 
+buildComparison <- function(df, field, value) {
+  dfsub <- df[df[,field] == value,]
+  colnames(dfsub)[7] <- 'comptime'
+  rv <- merge(df, dfsub[,-(1:3)])
+  rv <- transform(rv, xtime=time / comptime)
+}
 
-#listUploads()
-uploadName <- commandArgs(TRUE)
-cat(uploadName)
+
+args <- commandArgs(TRUE)
+cat(paste("args : ",args,""),sep="\n")
+args <- unlist(strsplit(args," "))
+uploadName <- args[1]
+#uploadName = "3m-moxi-oct-03"
+cat(paste("upload name : ",uploadName),sep="\n")
 pdf(paste(uploadName,sep="",".pdf"))
-#uploadName = "rampup-single-node-memcachetest-3m"
 df <- getUpload(uploadName)
 steps = c("loading-docs","loading-persisted")
-for(label in steps) {
-			d <- df[df$label == label,]
-			p <- ggplot(data=d,aes(x=build,y=time,fill=build))
-			p <- p + labs(y='Seconds', x="Build Number") 
-			p <- p + geom_bar(stat='identity', position='stack') 
-			p <- p + geom_text(aes(y = time,vjust=0,hjust=0,size=10,label=paste(sprintf("%.0f",time)," sec")))
-			p <- p + opts(title=paste(comma(d$items),prettySize(d$size, "%d"),"items -", label))
-			print(p)
+df <- transform(df,build=paste(gsub("couchbase-","",build)))
+df <- transform(df,build=paste(gsub("membase-","",build)))
+if (length(args) > 1) {
+	#there is a baseline
+	baseline <- args[2]
+#	baseline = "1.7.1r-68"
+	cat(paste("baseline : ",baseline),sep="\n")
+	df <- buildComparison(df, 'build', baseline)
+	for(label in steps) {
+				d <- df[df$label == label,]
+				p <- ggplot(data=d,aes(x=build,y=time,fill=build))
+				p <- p + labs(y='Seconds', x="Build Number") 
+				p <- p + geom_bar(stat='identity', position='stack') 
+				p <- p + geom_text(aes(y = time,vjust=0,hjust=1,label=paste(sprintf("%.2f",xtime)," X")))
+				p <- p + opts(legend.position = "none")
+				p <- p + opts(title=paste(comma(d$items),prettySize(d$size, "%d"),"items ", label))
+				p <- p + coord_flip()
+				p <- p + theme_bw()
+				print(p)
+	}
+	
+} else {
+	for(label in steps) {
+				d <- df[df$label == label,]
+				p <- ggplot(data=d,aes(x=build,y=time,fill=build))
+				p <- p + labs(y='Seconds', x="Build Number") 
+				p <- p + geom_bar(stat='identity', position='stack') 
+				p <- p + geom_text(aes(y = time,vjust=0,hjust=1,label=paste(sprintf("%.0f",time)," sec")))
+				p <- p + opts(legend.position = "none")
+				p <- p + opts(title=paste(comma(d$items),prettySize(d$size, "%d"),"items ", label))
+				p <- p + coord_flip()
+				p <- p + theme_bw()
+				print(p)
+	}
 }
 dev.off()
